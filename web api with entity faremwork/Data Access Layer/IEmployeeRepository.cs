ca081f59b94ffa036526e0c034ae50e7c0466ee8
MadhunikaLagishetty <104381581+MadhunikaLagishetty@users.noreply.github.com>
2022-05-04 21:33:48 +0530

    done

---
+++ web api with entity faremwork/Data Access Layer/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain_Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer
+{
+    public interface IEmployeeRepository
+    {
+        public Task<List<EmployeeDto>> GetEmployees();
+        public Task<EmployeeDto> GetEmployee(int id);
+        public Task InsertEmployee(EmployeeDto employee);
+        public Task UpdateEmployee(EmployeeDto employee);
+        public Task DeleteEmployee(int id);
+    }
+}
---
+++ web api with entity faremwork/Data Access Layer/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Data_Access_Layer.Models
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
